a8655a216acdc442fa94d63e5b30d56c2ad7694c
Priceben <huntingtonfighter@gmail.com>
2021-08-02 14:48:47 -0400

    nav.vue

---
+++ vue/src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="nav-container">
+      <router-link :to="{name: 'breweries'}">Breweries</router-link>
+      <router-link :to="{name: 'beerList'}">All Beers</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.nav-container{
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+}
+</style>
