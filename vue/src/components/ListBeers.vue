1f2b30140891286b4885d6ddec2a1b5797ece187
kale Curtis[D[D[D[D[D[D[Db[D[D[D[K[C[C[C[C [C[C[CCurtis <kalebcurtis54@gmail.com>
2021-08-03 11:08:41 -0400

    listbeer component

--- vue/src/components/ListBeers.vue
+++ vue/src/components/ListBeers.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <div class="list-beers">
+    <!-- Might want to make a grid view rather than use a table -->
+    <table>
+    <thead>
+      <tr>
+        <th>All Beers</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="beer in this.$store.state.beerList" v-bind:key="beer.id"></tr>
+      <router-link v-bind:to="{ name: 'beer', params:{id: beer.id}}" >{{ beer.name }}</router-link>
+    </tbody>
+  </table> 
+
+  </div>
 </template>
 
 <script>
+import beerService from "@/services/BeerService.js";
+
 export default {
+  name: 'beer-list',
+  methods: {
+    getBeerList(){
+      beerService.getBeerList().then(response => {
+        this.$store.commit("SET_BEERLIST", response.data);
+      });
+    }
+  }
 
 }
 </script>
