b8b4187ab8ae8522ce717261be39a203edfc1d20
Priceben <huntingtonfighter@gmail.com>
2021-08-04 09:51:03 -0400

    fixed BreweryList store

--- vue/src/components/BreweryList.vue
+++ vue/src/components/BreweryList.vue
@@ -1,22 +1,10 @@
 <template>
 <div>
 <div class="brewery-list">
-  <!-- <table>
-    <thead>
-      <tr>
-        <th>All Breweries</th>
-      </tr>
-    </thead>
-    <tbody>
-        <tr v-for="brewery in breweries" v-bind:key="brewery.breweryId"></tr>
-      <td>
+  <h1>All Breweries</h1>
+    <div v-for="brewery in this.$store.state.breweries" v-bind:key="brewery.breweryId">
         <router-link v-bind:to="{ name: 'brewery', params:{id: brewery.breweryId}}" >{{ brewery.name }}</router-link>
-      </td>
-    </tbody>
-  </table> -->
-  <div v-for="brewery in breweries" v-bind:key="brewery.breweryId">
-    <router-link :to="{ name: 'brewery', params:{id: brewery.breweryId}}">{{ brewery.name}}</router-link>
-  </div>
+    </div>
 </div>
 </div>
   
@@ -27,26 +15,18 @@ import beerService from "@/services/BeerService.js";
 
 export default {
   name: 'brewery-list',
-  data() {
-    return {
-      breweries: [],
-    };
-  }, 
-  /* methods: {
+  methods: {
     getBreweries(){
       beerService.getBreweries().then(response => {
         this.$store.commit("SET_BREWERIES", response.data);
       });
     }
-  }, */
+  }, 
   created(){
-    beerService.getBreweries().then(response => {
-      this.breweries = response.data;
-    }); 
-    /* this.getBreweries();
-  } */
+    this.getBreweries();
+  }
 }
-}
+
 </script>
 
 <style>
